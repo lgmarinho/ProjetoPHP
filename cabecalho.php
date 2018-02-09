037c38d8d1d99ad5525552681509489ec038106b
luizgustavo <luizgustavo@gaylussac.com.br>
2018-02-09 12:21:29 -0200

    Implementação de Tipo de Alerta no PHP

--- cabecalho.php
+++ cabecalho.php
@@ -1,28 +1,29 @@
 <html>
 
+
 <head> 
 	<title>Loja do Gustavo</title>
 	<meta charset="utf-8">
-	<link href="css/bootstrap.css" rel="stylesheet">
+   <link href="css/bootstrap.css" rel="stylesheet">
 	<link href="css/bootstrap-reboot.css" rel="stylesheet">
 	<link href="css/loja.css" rel="stylesheet">
 	<link href="css/bootstrap-grid.css" rel="stylesheet">
-
 	</head>
 
 
 <body>
-	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+   <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
          <ul class="navbar-nav">
             <li class="nav-item active">
                <a class="nav-link" href="index.php">Inicio</a>
             </li>
-            <li class="nav-item active">
-               <a class="nav-link" href="ProdutoForm.php">Adicionar Produto</a>
-            </li>
-            <li class="nav-item active">
-               <a class="nav-link" href="ProdutoLista.php">Lista de Produtos</a>
-            </li>            
+            <div class="dropdown show">
+                 <a class="btn btn-primary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Produto </a>
+                 <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                     <a class="dropdown-item"  href="ProdutoForm.php">Adicionar Produto</a>
+                     <a class="dropdown-item" href="ProdutoLista.php">Lista de Produtos</a>                     
+                 </div>
+            </div>          
         </ul>
 </nav>
 
